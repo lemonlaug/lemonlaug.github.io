860363cc106fd61a5f3f0e900adc452eae1740bb
Isaac Laughlin <isaac.laughlin@galvanize.com>
2016-06-02 18:31:42 -0700

    Adding js password fakery.

---
+++ js/pass.js
@@ -0,0 +1,16 @@
+hashCode = function(s){
+  return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+}
+
+$(document).ready(function () {
+    $('#password_field').keydown(function(event) {
+        if (event.keyCode == 13) {
+	    if (hashCode($('#password_field').val())== -850947951) {
+		$('#hidden_area').show('slow');
+		$('#password_area').hide('fast');
+	    } else {
+		$('#password_label').text("Try again: ");
+	    }
+	}
+    });
+});

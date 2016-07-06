a966e750e31a07d42cc1c9ea36be4a6e11ed863c
Isaac Laughlin <isaac.laughlin@galvanize.com>
2016-07-06 15:12:22 -0700

    Changing password, and making it not case-sensitive.

--- js/pass.js
+++ js/pass.js
@@ -5,7 +5,7 @@ hashCode = function(s){
 $(document).ready(function () {
     $('#password_field').keydown(function(event) {
         if (event.keyCode == 13) {
-	    if (hashCode($('#password_field').val())== -850947951) {
+	    if (hashCode($('#password_field').val().toLowerCase())== 104080927) {
 		$('#hidden_area').show('slow');
 		$('#password_area').hide('fast');
 	    } else {
